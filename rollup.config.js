ccc505ce0372354a2e14ad990680f290e029f9c1
nicolasmondon <nicolasmondon@gmail.com>
2018-01-22 22:46:18 +0100

    setup

---
+++ rollup.config.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
+const resolve = require('rollup-plugin-node-resolve');
+const pkg = JSON.parse(fs.readFileSync('package.json', 'utf-8'));
+
+module.exports = {
+    input: 'src/index.js',
+    output: [
+        {
+            file: pkg.main,
+            format: 'cjs'
+        },
+        {
+            file: pkg.module,
+            format: 'es'
+        }
+    ],
+    sourceMap: false,
+    external: ['d3-hierarchy', 'd3-scale'],
+    plugins: [babel(), commonjs(), resolve()]
+};
